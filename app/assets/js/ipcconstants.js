6c91e4b35962e183d140828803b5f75820e275a3
Seb <sebbyd05@gmail.com>
2022-11-29 10:45:30 -0600

    Changed Azure ID
    Now, it works, not that it didn't before.

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
+exports.AZURE_CLIENT_ID = 'c880a0c4-6a3d-4c2b-8d2e-50367eba8b11'
 // SEE NOTE ABOVE.
 
 
